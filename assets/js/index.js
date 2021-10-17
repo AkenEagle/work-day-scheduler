dc7b4caf12154cf2dbf37319c33c4a0afe684c85
AkenEagle <akeneagle@gmail.com>
2021-10-17 15:35:04 +0100

    Timeblock styling

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,9 +3,9 @@ const buildTimeBlocks = function () {
     const times = [`9AM`, `10AM`, `11AM`, `12PM`, `1PM`, `2PM`, `3PM`, `4PM`, `5PM`];
 
     for(let i=0; i<times.length; i++) {
-        const divTimeBlock = $(`<div class="jumbotron"></div>`);
-        const divTBTime = $(`<div class="timeblock-time">${times[i]}</div>`);
-        const divTBTextArea = $(`<textarea class="timeblock-textarea"></textarea>`);
+        const divTimeBlock = $(`<div class="row"></div>`);
+        const divTBTime = $(`<div class="hour">${times[i]}</div>`);
+        const divTBTextArea = $(`<textarea class="description"></textarea>`);
         const divSaveBTN = $(`<div class="saveBtn"><i class="fas fa-save"></i></div>`);
 
         divTimeBlock.append(divTBTime);
